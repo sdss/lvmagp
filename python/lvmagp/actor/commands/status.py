fe0f035fd0d64429aaa799f4ada8ab9d295a1433
Florian Briegel <wasndas@zone42.de>
2022-04-22 13:09:35 +0200

    added missing status command

---
+++ python/lvmagp/actor/commands/status.py
@@ -0,0 +1,24 @@
+import asyncio
+
+import click
+import numpy as np
+from clu.command import Command
+
+from . import parser
+from lvmagp.actor.statemachine import ActorState, ActorStateMachine
+from lvmagp import exceptions
+
+
+@parser.command("status")
+async def status(
+    command: Command,
+    telsubsystems,
+):
+    """Status information"""
+    actor_statemachine = command.actor.statemachine
+    
+    status = {
+        "state": actor_statemachine.state.value,
+    }    
+
+    return command.finish(status)
