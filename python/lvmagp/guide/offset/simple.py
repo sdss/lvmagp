bfd0a4b3d6c465b5b3c1e5ee5a7a8abc23c6c5d8
Florian Briegel <briegel@mpia.de>
2022-09-26 16:35:52 +0200

    added famous cos(deg)

--- python/lvmagp/guide/offset/simple.py
+++ python/lvmagp/guide/offset/simple.py
@@ -1,6 +1,8 @@
 from typing import Tuple, Dict, List, Any
-import numpy as np
 import logging
+
+import numpy as np
+from math import cos
 
 from lvmagp.images import Image
 from lvmagp.images.processors.detection import SourceDetection
@@ -21,6 +23,8 @@ class GuideOffsetSimple(GuideOffset):
 
         self.source_detection: SourceDetection = source_detection()
         self.reference_centroids = None
+        self.max_sources = 42
+        self.search_boxsize = 9
 
 
     async def reference_images(self, images: List[Image]) -> None:
@@ -35,27 +39,41 @@ class GuideOffsetSimple(GuideOffset):
             sources = image.catalog
             sources.sort("peak")
             sources.reverse()
-            sources = sources[:42]
+            sources = sources[:self.max_sources]
             ref = np.array([sources['x'], sources['y']]).transpose()
-            self.reference_centroids.append(np.array([centroid_quadratic(img.data, xpeak=x, ypeak=y, search_boxsize=9) for x,y in ref]))
+            self.reference_centroids.append(
+                np.array(
+                    [centroid_quadratic(img.data, xpeak=x, ypeak=y, search_boxsize=self.search_boxsize) 
+                        for x, y in ref]))
+        print(f"{len(self.reference_centroids[0])} {len(self.reference_centroids[1])}")
 
-
-
-    async def find_offset(self, images: List[Image]) -> Tuple[float, float]:
+    async def find_offset(self, images: List[Image]) -> float:
         """ Find guide offset """
 
         diff_centroids = []
 
         try:
-            for idx, img in enumerate(images):
-                current_centroids = [centroid_quadratic(img.data, xpeak=x, ypeak=y, search_boxsize=7) for x,y in self.reference_centroids[idx]]
-                diff_centroids.append(self.reference_centroids[idx]-np.array(current_centroids))
-
-#            print(f"{diff_centroids}")
-            print(f"west: {np.median(diff_centroids[0], axis=0)} east: {np.median(diff_centroids[1], axis=0)}")
+            for im_idx, img in enumerate(images):
+                ref_cen = self.reference_centroids[im_idx]
+                diff_cen = []
+                for c_idx, xy in enumerate(ref_cen):
+                   x, y = xy
+                   centroid = centroid_quadratic(img.data, xpeak=x, ypeak=y, search_boxsize=self.search_boxsize)
+                   if not np.isnan(centroid).any():
+                       diff_cen.append(ref_cen[c_idx] - centroid)
+                diff_centroids.append(np.array(diff_cen))
+            print(f"{images[0].header['RA']/15}h:{images[0].header['DEC']}d")
+#            print(f"{images[0].header['PIXELSC']}, {images[0].header['BINX']}")
+            print(f"{images[0].header['CAMNAME']}: {np.median(diff_centroids[0], axis=0)} {images[1].header['CAMNAME']}: {np.median(diff_centroids[1], axis=0)}")
+            diff =  np.median(np.concatenate((diff_centroids[0], diff_centroids[1])), axis=0)
+            diff *= [images[0].header['BINX'], images[0].header['BINY']]
+            diff *= [images[0].header['PIXELSC'], images[0].header['PIXELSC']]
+            diff /= [cos( np.deg2rad(images[0].header['DEC'])), 1.0]
+            return diff
+        
 
         except Exception as ex:
-            print(f"error {ex}")
+            print(f"error: {type(ex)} {ex}")
 
 
 __all__ = ["GuideOffsetSimple"]
