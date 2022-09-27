d70a93c4d829557c9e201fa6e9d797eb5f354a7f
Florian Briegel <briegel@mpia.de>
2022-09-27 15:33:37 +0200

    added python astrometry

--- python/lvmagp/guide/worker.py
+++ python/lvmagp/guide/worker.py
@@ -71,6 +71,47 @@ class GuiderWorker():
             return images
 
 
+    async def offset_mount(self, offset):
+            """
+            One or more of the following offsets can be specified as a keyword argument:
+
+            AXIS_reset: Clear all position and rate offsets for this axis. Set this to any value to issue the command.
+            AXIS_stop_rate: Set any active offset rate to zero. Set this to any value to issue the command.
+            AXIS_add_arcsec: Increase the current position offset by the specified amount
+            AXIS_set_rate_arcsec_per_sec: Continually increase the offset at the specified rate
+
+            Where AXIS can be one of:
+
+            ra: Offset the target Right Ascension coordinate
+            dec: Offset the target Declination coordinate
+            axis0: Offset the mount's primary axis position
+                (roughly Azimuth on an Alt-Az mount, or RA on In equatorial mount)
+            axis1: Offset the mount's secondary axis position
+                (roughly Altitude on an Alt-Az mount, or Dec on an equatorial mount)
+            path: Offset along the direction of travel for a moving target
+            transverse: Offset perpendicular to the direction of travel for a moving target
+
+            For example, to offset axis0 by -30 arcseconds and have it continually increase at 1
+            arcsec/sec, and to also clear any existing offset in the transverse direction,
+            you could call the method like this:
+
+            mount_offset(axis0_add_arcsec=-30, axis0_set_rate_arcsec_per_sec=1, transverse_reset=0)
+
+            """
+            try:
+                self.logger.info(f"{offset}")
+                if self.statemachine.state == ActorState.GUIDE and (abs(offset) > [0.5, 0.5]).any():
+                    offset *= -0.95
+                    self.logger.debug(f"correcting {offset}")
+                    #
+#                    await self.telsubsystems.pwi.offset(axis0_add_arcsec = offset[0], axis1_add_arcsec = offset[1])
+                    await self.telsubsystems.pwi.offset(ra_add_arcsec = offset[0], dec_add_arcsec = offset[1])
+
+
+            except Exception as e:
+                self.logger.error(e)
+                raise e
+
     async def work(self, exptime=nan, pause=False):
 
         try:
@@ -85,12 +126,8 @@ class GuiderWorker():
             while self.statemachine.state in (ActorState.GUIDE, ActorState.PAUSE):
 
                 images = await self.expose(exptime)
-                offsets = await self.offsetcalc.find_offset(images)
-                self.logger.info(f"{offsets}")
-                if self.statemachine.state == ActorState.GUIDE and (abs(offsets) > [0.5, 0.5]).any():
-                    offsets *= -0.95
-                    self.logger.debug(f"correcting {offsets}")
-                    await self.telsubsystems.pwi.offset(ra_add_arcsec = offsets[0], dec_add_arcsec = offsets[1])
+                offset = await self.offsetcalc.find_offset(images)
+                await self.offset_mount(offset)
                 await asyncio.sleep(1.0)
 
         except Exception as e:
---
+++ utils/astrometrynet_test.py
@@ -0,0 +1,110 @@
+# 2 choices
+
+# 1. with simulator on nicelab, user lvm
+#   RMQ_HOST=$(minikube ip) ipython3
+#   paste the code
+
+# 2. local with image eg from nicelab:  /data/lvm/sci/agcam/east/20220927/lvm.sci.agcam.east_00003730.fits
+#    python > 3.8
+#    pip3 install sdss-lvmagp astrometry
+#    ipython3
+
+from logging import DEBUG
+
+from sdsstools.logger import StreamFormatter
+from sdsstools import get_logger, read_yaml_file
+from sdsstools.logger import SDSSLogger
+
+import numpy as np
+
+from astropy.coordinates import SkyCoord, Angle
+import astropy.wcs
+import astropy.units as u
+
+from lvmtipo.actors import lvm
+from lvmagp.images import Image
+from lvmagp.images.processors.detection import DaophotSourceDetection, SepSourceDetection
+
+log = get_logger("astro")
+log.sh.setLevel(DEBUG)
+log.sh.formatter = StreamFormatter(fmt='%(asctime)s %(name)s %(levelname)s %(filename)s:%(lineno)d: \033[1m%(message)s\033[21m')
+log.debug("test")
+
+import astrometry
+solver = astrometry.Solver(
+    astrometry.series_5200.index_files(
+        cache_directory="astrometry_cache",
+        scales={6},
+    )
+)
+
+#source_detection = DaophotSourceDetection()
+source_detection = SepSourceDetection()
+
+# only first iteration
+logodds_callback = astrometry.Action.STOP
+# full
+#logodds_callback = astrometry.Action.CONTINUE
+
+#  choice 1.
+log.debug("Expose")
+
+tssn="sci"
+exptime=3.0
+
+telsubsys = await lvm.from_string(tssn).start()
+rc = await telsubsys.agc.expose(exptime)
+images = [ Image.from_file(v["filename"]) for k,v in rc.items() ]
+
+
+# choice 2.
+log.debug("Load")
+filename = "/data/lvm/sci/agcam/east/20220927/lvm.sci.agcam.east_00003730.fits"
+
+images = [ Image.from_file(filename)]
+
+log.debug("Detect")
+source_count=17
+
+image = await source_detection(images[0])
+arcsec_per_pixel=1/image.header['PIXELSC']*image.header['BINX']
+
+if image.catalog is None:
+    log.warning("No catalog found in image.")
+
+log.debug("Sort")
+sources = image.catalog
+sources.sort("peak")
+sources.reverse()
+sources = sources[:source_count]
+
+log.debug("Solve start")
+solution = solver.solve(
+    stars_xs=sources['x'],
+    stars_ys=sources['y'],
+    size_hint=astrometry.SizeHint(
+        lower_arcsec_per_pixel=arcsec_per_pixel-0.1,
+        upper_arcsec_per_pixel=arcsec_per_pixel+0.1,
+    ),
+    position_hint=astrometry.PositionHint(
+        ra_deg=image.header['RA'],
+        dec_deg=image.header['DEC'],
+        radius_deg=1,
+    ),
+    solve_id=None,
+    tune_up_logodds_threshold=14.0, # None disables tune-up (SIP distortion)
+    output_logodds_threshold=21.0,
+    logodds_callback=lambda logodds_list: logodds_callback
+)
+log.debug("Solve done")
+
+if solution.has_match():
+    print(f"{solution.best_match().center_ra_deg=}")
+    print(f"{solution.best_match().center_dec_deg=}")
+    print(f"{solution.best_match().scale_arcsec_per_pixel=}")
+    wcs = astropy.wcs.WCS(solution.best_match().wcs_fields)
+    pixels = wcs.all_world2pix(
+        [[star.ra_deg, star.dec_deg] for star in solution.best_match().stars],
+        0,
+    )
+    print(f"{wcs}")
